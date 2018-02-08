6f1cae75da5527def81b5cce0952965b105446fa
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-07 17:56:50 -0700

    Create index route to use on /

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const bodyParser = require('bodyParser');
 const config = require('./api/util/config');
 const logger = require('./api/util/logger');
 
+const index = require('./api/routes/index');
+
 const app = express();
 const server = require('http').Server(app);
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', index);
 
 server.listen(config.port, () => {
   logger.info(`txtSmrt API listening on port ${config.port}`);
