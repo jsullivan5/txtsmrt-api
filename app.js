414645f2ba903da640b144e9b52818972211f92c
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-07 20:39:15 -0700

    Add SMS route to app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require('./api/util/config');
 const logger = require('./api/util/logger');
 
 const index = require('./api/routes/index');
+const sms = require('./api/routes/sms.route');
 
 const app = express();
 const server = require('http').Server(app);
@@ -14,7 +15,9 @@ const server = require('http').Server(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
 app.use('/', index);
+app.use('/sms', sms);
 
 server.listen(config.port, () => {
   logger.info(`txtSmrt API listening on port ${config.port}`);
