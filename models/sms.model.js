57fe55560931c02e4d8cd98ae09e7afc10bdd3bf
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-08 20:47:00 -0700

    Add SMS model

---
+++ models/sms.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SmsSchema = new Schema({
+  to: String,
+  from: String,
+  text: String,
+  smsId: String
+});
+
+module.exports = mongoose.model('Sms', SmsSchema);
