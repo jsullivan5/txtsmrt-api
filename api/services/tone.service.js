53db00f1bd494d2653319c181390c18705aaa815
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-15 13:33:34 -0700

    Add tone formatting to tone service

--- api/services/tone.service.js
+++ api/services/tone.service.js
@@ -25,12 +25,39 @@ class ToneService {
           logger.error('error:', error);
           reject(error);
         } else {
-          const parsedResponse = JSON.stringify(response, null, 2);
-          logger.debug(parsedResponse);
-          resolve(parsedResponse);
+          logger.debug('Tones received from Watson');
+          resolve(response);
         }
       });
     });
+  }
+
+  formatToneArray (toneArray) {
+    return toneArray.sort((a, b) => (b.score * 100) - (a.score * 100))
+      .map(tone => (`${tone.tone_name} : ${(tone.score * 100).toFixed(2)}%`));
+  }
+
+  getIntroText (toneArray) {
+    const sorted = toneArray.sort((a, b) => (b.score * 100) - (a.score * 100));
+
+    if (sorted[0].score === 0) {
+      return 'Try a longer message.';
+    }
+
+    switch (sorted[0].tone_name) {
+    case 'Joy':
+      return 'You sound mostly joyful.';
+    case 'Anger':
+      return 'You sound mostly angry.';
+    case 'Sadness':
+      return 'You sound mostly sad.';
+    case 'Fear':
+      return 'You sound mostly afraid.';
+    case 'Disgust':
+      return 'You sound mostly disgusted.';
+    default:
+      return '';
+    }
   }
 }
 
