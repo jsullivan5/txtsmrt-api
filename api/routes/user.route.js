d35db2fa91c7ec5ee012d65e4d57b7c9130571d9
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-16 11:56:30 -0700

    Add Login and logout endpoints to user route. Remove passwordConf from User obj.

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,14 +6,20 @@ router.post('/signup', async (req, res, next) => {
   const { phoneNumber, email, firstName, lastName, password, passwordConf } = req.body;
   let userData;
 
+  if (password !== passwordConf) {
+    const error = new Error('Passwords do not match.');
+    error.status = 400;
+    res.send('Passwords do not match');
+    return next(error);
+  }
+
   if (phoneNumber && email && firstName && lastName && password && passwordConf) {
     userData = {
       phoneNumber,
       email,
       firstName,
       lastName,
-      password,
-      passwordConf
+      password
     };
   }
 
@@ -24,6 +30,32 @@ router.post('/signup', async (req, res, next) => {
       return res.status(200).send('User Created');
     }
   });
+});
+
+router.post('/login', (req, res, next) => {
+  const { phoneNumber, password } = req.body;
+  User.authenticate(phoneNumber, password, function (error, user) {
+    if (error || !user) {
+      const err = new Error('Phone number or password incorrect');
+      err.status = 401;
+      return next(err);
+    } else {
+      req.session.userId = user._id;
+      return res.send(user);
+    }
+  });
+});
+
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy((error) => {
+      if (error) {
+        return next(error);
+      } else {
+        return res.send('Logged out');
+      }
+    });
+  }
 });
 
 module.exports = router;
