64d0583495485889b20e1dd5d478721197fc7fb5
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-20 21:46:00 -0700

    Add getUserByPhoneNumber in the Sms route to set user Id with SMS enrty in the database

--- api/routes/sms.route.js
+++ api/routes/sms.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const twilioService = require('../services/twilio.service');
 const smsService = require('../services/sms.service');
 const toneService = require('../services/tone.service');
+const userService = require('../services/user.service');
 const logger = require('../util/logger');
 
 const router = express.Router();
@@ -10,11 +11,12 @@ router.post('/', async (req, res, next) => {
   let toneMessage;
 
   try {
+    const user = await userService.getUserByPhoneNumber(req.body.From);
     const tones = await toneService.getTone(req.body.Body);
     toneMessage = await twilioService.formatMessage(
       tones.document_tone.tone_categories[0].tones
     );
-    await smsService.insertMessage(req.body);
+    await smsService.insertMessage(req.body, user);
     logger.debug(`Message inserted: [${req.body.Body}]`);
   } catch (error) {
     logger.error(`Error receiving SMS: [${error}]`);
